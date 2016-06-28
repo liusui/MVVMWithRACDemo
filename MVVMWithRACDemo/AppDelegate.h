7272bf9b00be8e23f190acdce11bcc8239fc16ae
liusui <952923202@qq.com>
2016-06-28 13:44:50 +0800

    Initial Commit

---
+++ MVVMWithRACDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMWithRACDemo
+//
+//  Created by Liusui on 16/6/28.
+//  Copyright © 2016年 Shanghai Elephant Financial Services Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
